1745727c47850f75cf9d93daad7b9c93e5708f2d
leo <cwq0312@163.com>
2013-02-23 15:36:43 +0800

    增加存储插件及WebSocket插件
    Signed-off-by: leo <cwq0312@163.com>

---
+++ src/Qmik.Store.js
@@ -0,0 +1,102 @@
+﻿/**
+//存储框架
+包含localStorage,sessionStorage,cookie
+*/
+; (function ($) {
+    //声明
+    var local = localStorage,
+        session = sessionStorage,
+        win = window,
+        doc = document;
+    function setCookie(key, value, ttl, domain) {//两个参数，一个是cookie的名子，一个是值
+        ttl = ttl || 30 * 24 * 60 * 60;
+        var exp = new Date(),
+            str;
+        str = key + "=" + escape(value);
+        if (domain) str += ";domain=" + domain;
+        if (ttl < 0) {
+            exp.setTime(exp.getTime() - 10)
+        } else {
+            exp.setTime(exp.getTime() + ttl * 1000)
+        }
+        str += ";expires=" + exp.toGMTString();
+        doc.cookie = str
+    }
+    function getCookie(key) {//取cookies函数        
+        var arr = doc.cookie.match(new RegExp("(^| )" + key + "=([^;]*)(;|$)"));
+        return arr != null ? unescape(arr[2]) : null;
+    }
+    function rmCookie(key, domain) {//删除cookie
+        setCookie(key, "", -1, domain)
+    }
+    function parseJSON(value) {
+        try {
+            return JSON.parse(value)
+        } catch (e) {
+            return value
+        }
+    }
+    function ttlTime(ttl) {
+        return ttl < 0 ? ttl : new Date().getTime() + ttl * 1000;
+    }
+    var Store = {
+        setLocal: function (key, value, ttl) {//储存到localStorage,ttl:unit is second
+            ttl = ttl || -1;
+            var item = { __data: value, __ttl: ttlTime(ttl) };
+            local && local.setItem(key, JSON.stringify(item))
+        },
+        setSession: function (key, value, ttl) {//储存到sessionStorage,ttl:unit is second
+            ttl = ttl || -1;
+            var item = { __data: value, __ttl: ttlTime(ttl) };
+            session && session.setItem(key, JSON.stringify(item))
+        },
+        setCookie: function (key, value, ttl, domain) {//储存到cookie,ttl:unit is second
+            setCookie(key, value, ttl, domain)
+        },
+        getLocal: function (key) {
+            var item = local ? parseJSON(local.getItem(key)) : null;
+            if (item && item.__data && item.__ttl) {
+                if (item.__ttl >= 0 && item.__ttl <= new Date().getTime()) {
+                    Store.rmLocal(key); return
+                }
+                return item.__data
+            }
+            return item
+        },
+        getSession: function (key) {
+            var item = session ? parseJSON(session.getItem(key)) : null;
+            if (item && item.__data && item.__ttl) {
+                if (item.__ttl >= 0 && item.__ttl <= new Date().getTime()) {
+                    Store.rmSession(key); return
+                }
+                return item.__data
+            }
+            return item
+        },
+        getCookie: function (key) {
+            getCookie(key)
+        },
+        rmLocal: function (key) {
+            local.removeItem(key)
+        },
+        rmSession: function (key) {
+            session.removeItem(key)
+        },
+        rmCookie: function (key, domain) {
+            rmCookie(key, domain)
+        },
+        clearLocal: function () {
+            local&&local.clear()
+        },
+        clearSession: function () {
+            session&&session.clear()
+        },
+        clearCookie: function () {
+            var arr = doc.cookie.match(new RegExp("(^| )" + key + "=([^;]*)(;|$)"));
+            for (var i in arr) {
+                setCookie(arr[i], "", -1, "")
+            }
+        }
+    }
+    $.Store = Store;
+})(Qmik);
---
+++ test/testStore.js
@@ -0,0 +1,95 @@
+﻿; (function () {
+    alert("test Storage ,please look console.log!!");
+
+    var win = window,
+        doc = document
+    ;
+    var Store = Qmik.Store;
+
+
+    function test() {
+        
+        var me = this;
+        var key1 = "mgk1",
+            key2 = "mgk2",
+            key3 = "mgk3",
+            key4 = "mgk4",
+            key5 = "mgk5",
+            key6 = "mgk6",
+             value;
+        me.val1 = "val1";
+        me.val2 = { name: "xiaohau", age: 25 };
+        me.val3 = "\r\nabcadsfsdf";
+        me.val4 = "<br/>adfasdf<br/>\r \n \t";
+        me.val5 = { bk: "<br/>adfasdf<br/>\r \n \t", mm: 109 };
+        me.val6 = { bk: "<br/>adfasdf<br/>\r \n \t", mm: 109, child: { name: "<<\\\\\\>>" } };
+
+
+
+        var count = 100;
+
+        localStorage.setItem("mmmgbc", "rereafasdfsadfasdfsadf[]<>adf");
+
+        console.log("------------test localStorage-------------------------------------------");
+        //test localStorage
+        for (var i = 1; i < count; i++) {
+            if (me["val" + i]) {
+                if (i % 3 == 0) {
+                    Store.setLocal("mgk" + i, me["val" + i],20);
+                } else {
+                    Store.setLocal("mgk" + i, me["val" + i]);
+                }
+            } 
+        }
+        for (var i = 1; i < count; i++) {
+            if (me["val" + i]) logLocal("mgk" + i);
+        }
+        
+        //test sessionStorage
+        console.log("///////////////////////////////////////////////////////////////////////");
+        console.log("------------test sessionStorage-------------------------------------------");
+        console.log("///////////////////////////////////////////////////////////////////////");
+        for (var i = 1; i < count; i++) {
+            if (me["val" + i]) Store.setSession("mgk" + i, me["val" + i]);
+        }
+        for (var i = 1; i < count; i++) {
+            if (me["val" + i]) logSession("mgk" + i);
+        }
+
+        //test cookie
+        console.log("///////////////////////////////////////////////////////////////////////");
+        console.log("------------test cookie ,cookie需要在域名下测试-------------------------------------------");
+        console.log("///////////////////////////////////////////////////////////////////////");
+        for (var i = 1; i < count; i++) {
+            if (me["val" + i]) Store.setCookie("mgk" + i, me["val" + i]);
+        }
+        for (var i = 1; i < count; i++) {
+            if (me["val" + i]) logCookie("mgk" + i);
+        }
+    }
+    function logLocal(key) {
+        var val = Store.getLocal(key);
+        if (val instanceof String) {
+            console.log("getValue>string>key:" + key + ":" + val);
+        } else {
+            console.log("getValue>json>key:" + key + ":" + JSON.stringify(val));
+        }
+    }
+    function logSession(key) {
+        var val = Store.getSession(key);
+        if (val instanceof String) {
+            console.log("getValue>string>key:" + key + ":" + val);
+        } else {
+            console.log("getValue>json>key:" + key + ":" + JSON.stringify(val));
+        }
+    }
+    function logCookie(key) {
+        var val = Store.getCookie(key);
+        if (val instanceof String) {
+            console.log("getValue>string>key:" + key + ":" + val);
+        } else {
+            console.log("getValue>json>key:" + key + ":" + JSON.stringify(val));
+        }
+    }
+    test();
+})();
