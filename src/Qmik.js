a6ff416c64255481d25c5ba594f7f7bad35e5014
leo <cwq0312@163.com>
2013-05-08 17:10:07 +0800

    调整

--- src/Qmik.js
+++ src/Qmik.js
@@ -4,7 +4,7 @@
  * @version:0.91.008
  */
 (function() {
-	var global = this, doc = global.document || {}, config = {};
+	var win = this, doc = win.document || {}, config = {};
 	var slice = Array.prototype.slice;
 	// define qmik object
 	function Q(selector, context) {
@@ -38,9 +38,9 @@
 			return this.toUpperCase()
 		}
 	});
-	function filter(callback) {
+	function filter(array, callback) {
 		var ret = [];
-		each(this, function(i, v) {
+		each(array, function(i, v) {
 			callback(v) && ret.push(v)
 		});
 		return ret
@@ -49,7 +49,7 @@
 	// ///////////////////////////////////////////////////////////////////
 	// isNull
 	function isNull(v) {
-		return v === undefined || v == null
+		return v === undefined || v === null
 	}
 	// isString
 	function isString(v) {
@@ -64,20 +64,27 @@
 		return v instanceof Array
 	}
 	function likeArray(v) { // like Array
-		return isArray(v) || (v && !isDom(v) && !isString(v) && isNum(v.length) && v != global)
+		return isArray(v) || (v && !isDom(v) && !isString(v) && isNum(v.length) && v != win)
 	}
 	// isFunction
 	function isFun(v) {
 		return v instanceof Function
+	}
+	function isError(v) {
+		return v instanceof Error
+	}
+	function isObject(v) {
+		return v instanceof Object
 	}
 	function each(obj, callback) { // each fun(k,v)
 		var i;
 		if (likeArray(obj)) for (i = 0; i < obj.length; i++) {
 			if (callback.call(obj[i], i, obj[i]) === !1) break
 		}
-		else if (isBS(obj) || isNum(obj) || isFun(obj)) callback.call(obj, i, obj);
-		else for (i in obj) {
-			if (callback.call(obj[i], i, obj[i]) === !1) break
+		else if (isObject(obj)) {
+			for (i in obj) {
+				if (callback.call(obj[i], i, obj[i]) === !1) break
+			}
 		}
 	}
 	// isNumber
@@ -87,11 +94,11 @@
 	function isBool(v) {
 		return typeof v == 'boolean'
 	}
-	function isBS(v) {
-		return isBool(v) || isString(v)
+	function isBaseType(v) {
+		return isBool(v) || isString(v) || isNum(v)
 	}
 	function toString(v) {
-		return isBS(v) || isNum(v) ? v : JSON.stringify(v)
+		return isBaseType(v) ? v : isFun(v) ? v.toString() : JSON.stringify(v)
 	}
 	// to json
 	function toJSON(v) {
@@ -100,7 +107,7 @@
 		return JSON.parse(v)
 	}
 	function isEvent(e) {
-		return global.Event && e instanceof global.Event || e == global.event
+		return win.Event && e instanceof win.Event || e == win.event
 	}
 	function execObject(v, target) {
 		return isFun(v) ? (target ? v.call(target, v) : v()) : v
@@ -113,6 +120,9 @@
 			})
 		}
 		return array
+	}
+	function isGrandfather(grandfather, child) {
+		return isDom(child) && (grandfather === child.parentNode ? !0 : isGrandfather(grandfather, child.parentNode))
 	}
 	Q.extend( {
 		encode : encodeURIComponent,
@@ -127,23 +137,20 @@
 		isArray : isArray,
 		isNull : isNull,
 		likeArray : likeArray,
+		isError : isError,
 		each : each,
 		stringify : toString,
 		parseJSON : toJSON,
-		// 合并数组或对象
-		merge : merge,
 		isEvent : isEvent,
 		likeArray : function(v) { // like Array
-			return isArray(v) || (v && !isDom(v) && !isString(v) && isNum(v.length) && v != global)
+			return isArray(v) || (v && !isDom(v) && !isString(v) && isNum(v.length) && v != win)
 		},
 		isDate : function(v) {
 			return v instanceof Date
 		},
-		isObject : function(v) {
-			return v instanceof Object
-		},
+		isObject : isObject,
 		isPlainObject : function(v) { // isPlainObject
-			if (isNull(v) || v + '' != '[object Object]' || v.nodeType || v == global) return !1;
+			if (isNull(v) || v + '' != '[object Object]' || v.nodeType || v == win) return !1;
 			var k;
 			for (k in v) {
 			}
@@ -179,25 +186,22 @@
 		toUpper : function(v) {
 			return v ? v.toUpper() : v
 		},
+		// 合并数组或对象
+		merge : merge,
 		array : function(array) {
 			return merge( [], array)
 		},
 		inArray : function(value, array) {
 			if (Q.likeArray(array)) for ( var i = 0; i < array.length; i++)
-				if (array[i] === value) i;
+				if (array[i] === value) return i;
 			return -1
 		},
 		unique : function(array) {
 			for ( var i = array.length - 1, j; i >= 0; i--)
 				for (j = i - 1; j >= 0; j--)
-					if (array[i] === array[j]) array.splice(i, 1)
+					array[i] === array[j] && array.splice(i, 1)
 		},
-		contains : function(p, c) {
-			if (isDom(p) && isDom(c)) return p === c || NGP(p, c);
-			for ( var k in p)
-				if (p[k] === c) return !0;
-			return !1
-		},
+		contains : isGrandfather,
 		map : function(array, callback) {
 			var r = [];
 			each(array, function(i, v) {
@@ -205,27 +209,27 @@
 			});
 			return r
 		},
-		getScript : function(url, callback,async) {
+		getScript : function(url, callback) {
 			var s = doc.createElement("script");
 			s.type = "text/javascript";
 			s.src = url;
-			s.defer="defer";
+			s.defer = "defer";
 			Q(doc.head).append(s);
-			s.onload=s.onreadystatechange = callback
+			s.onload = s.onreadystatechange = callback
 		},
-		serialize : function(a) {
-			return Q.param(Q.serializeArray(a))
+		serialize : function(array) {
+			return Q.param(Q.serializeArray(array))
 		},
-		serializeArray : function(a) {
-			return toV(a, function(v) {
+		serializeArray : function(array) {
+			return filter(array, function(v) {
 				return v && v.name ? {
 					name : v.name,
 					value : execObject(v.value)
-				} : !1;
+				} : !1
 			})
 		},
 		grep : function(array, callback) {
-			return filter.call(array, function(v) {
+			return filter(array, function(v) {
 				return callback ? callback(v) : !isNull(v)
 			})
 		},
@@ -237,7 +241,7 @@
 			});
 			return h.join('&')
 		},
-		time : function(d) {
+		now : function(d) {
 			return (d || 0) + new Date().getTime()
 		},
 		// 延迟执行,==setTimeout
@@ -250,24 +254,18 @@
 		// 周期执行,==setInterval
 		cycle : function(fun, time) {
 			var params = slice.call(arguments, 2);
-			function _exec(){
-				fun.apply(fun, params)
-				Q.delay(_exec,time);
+			function _exec() {
+				fun.apply(fun, params);
+				Q.delay(_exec, time)
 			}
-			Q.delay(_exec,time)
+			Q.delay(_exec, time)
 		},
 		log : function(msg, e) {
-			if (Q.config().debug) {
-				var m;
-				if (msg instanceof Error) {
-					e = msg;
-					m = e.stack;
-					msg = undefined
-				} else {
-					m = msg || ""
-				}
+			if (config.debug) {
+				msg = isError(msg) ? msg.stack : msg;
+				msg += isError(e) ? e.stack : "";
 				try {
-					console.log(m)
+					console.log(msg)
 				} catch (e) {
 				}
 			}
@@ -282,14 +280,14 @@
 			return /Windows Phone/.test(UA)
 		},
 		config : function(key, value) {
-			if (isPlainObject(k)) Q.extend(config, k);
+			if (isObject(key)) Q.extend(config, key);
 			else if (arguments.length > 1) config[key] = value;
-			return isString(key) ? config[key] : Q
+			return isString(key) ? config[key] : config
 		}
 	});
 	Q.version = "1.00.001";
-	Q.global = global;
-	global.Qmik = Q;
-	global.$ = global.$ || Q;
+	Q.global = win;
+	win.Qmik = Q;
+	win.$ = win.$ || Q;
 	return Q;
 })();
--- src/Qmik.sun.js
+++ src/Qmik.sun.js
@@ -5,102 +5,195 @@
  */
 (function(Q) {
 	var win = Q.global, loc = win.location, each = Q.each;
-	var cache = {};
 	var isArray = Q.isArray, isString = Q.isString, isFun = Q.isFun;
-	alias = {};
-	paths = {};
-	vars = {};
-	map = [];
-	config = {};
-	cache = {};
+	var config = {
+		alias : {},
+		paths : {},
+		vars : {},
+		map : [],
+		preload : []
+	};
+	var cacheModule = {};
 	base = loc.protocol + "//" + loc.pathname.replace(/\/[^\/]*$/, "/").replace(/\/\s*[0-9.]*\s*\/$/, "/")
 	function sun() {
+	}
+	function Module(id, dependencies, factory) {
+		var me = this;
+		me.url = id2url(id);
+		me.id = id;
+		me.dependencies = dependencies;
+		me.factory = factory;
+		me.isReady = !1;//module is ready ,if no, request src from service
+		me.exports = {}
 	}
 	Q.extend(sun, {
 		use : use,
 		// factory:function(require, exports, module)
 		define : define,
-		resolve : id2uri,
-		config : function(key, value) {
-			if (Q.isPlainObject(k)) Q.extend(config, k);
-			else if (arguments.length > 1) config[key] = value;
-			return isString(key) ? config[key] : this
+		resolve : id2url,
+		config : function(opts) {
+			Q.extend(config, opts);
+			return isString(opts) ? config[opts] : this
 		}
 	});
 	// factory:function(require, exports, module)
-	function define(id, deps, factory) {
-		if (isFun(deps)) {
-			factory = depts;
-			deps = null
+	function define(id, dependencies, factory) {
+		if (isFun(id)) {
+			factory = id;
+			dependencies = [];
+			id = null;
+		} else if (isFun(dependencies)) {
+			var url = id2url(id);
+			factory = dependencies;
+			dependencies = []
 		}
-		var module = {
-			exports : {}
-		};
-		factory(require, module.exports, module);
-		alias[id] = module.exports;
-		return alias[id]
+		dependencies = dependencies.concat(parseDepents(factory));
+		cacheModule[url] = new Module(id, dependencies, factory)
+	}
+	//get depends from function.toString()
+	function parseDepents(code) {
+		code = code.toString();
+		var params = code.replace(/^\s*function\s*\w*\s*/, "").match(/^\([\w ,]*\)/)[0].replace("\(", "")
+			.replace("\)", "");
+		var idx = params.indexOf(",");
+		if (idx == -1) return [];
+		var require = params.substring(0, idx);
+		var pattern = new RegExp(require + "\s*[(]\s*[\"']([^\"'\)]+)[\"']\s*[)]", "g");
+		var match = code.match(pattern);
+		match = Q.map(match, function(i, v) {
+			return v.replace(new RegExp("^" + require + "\s*[(]\s*[\"']"), "").replace(/\s*[\"']\s*[)]$/, "")
+		});
+		match = Q.map(match, function(i, v) {
+			return id2url(v)
+		});
+		return match
 	}
 	function use(ids, callback) {
 		var params = [];
 		if (isArray(ids)) {
+			var idx = 0;
 			each(ids, function(i, id) {
-				load(ids, function(exports) {
-					params.push(exports)
-				});
+				preload(function() {
+					load(ids, function(exports) {
+						params.push(exports);
+						if (++idx == ids.length) {
+							callback && callback.apply(callback, params);
+							delete idx, params
+						}
+					})
+				})
 			})
 		} else {
-			load(ids, function(exports) {
-				params.push(exports)
+			preload(function() {
+				load(ids, function(exports) {
+					params.push(exports);
+					callback && callback.apply(callback, params);
+					delete params
+				})
 			})
 		}
-		callback.apply(callback, params)
 	}
 	function require(id) {
-		return load(id);
+		return cacheModule[url]
 	}
-	require.async = function(ids, callback) {
-		Q.delay(function() {
+	Q.extend(require, {
+		resolve : id2url,
+		async : function(ids, callback) {
 			use(ids, callback)
-		}, 1)
-	}
-	function load(url, callback, async) {
-		if (cache[id]) {
-			callback(cache[id])
+		}
+	});
+	function preload(callback) {
+		var url, idx = 0, dependencies = config.preload, length = dependencies.length, depModule;
+		if (length == 0) {
+			callback()
 		} else {
-			request(id2uri(id), function() {
-				callback(cache[id])
-			}, async)
+			each(dependencies, function(i, v) {
+				url = id2url(v);
+				request(url, function() {
+					depModule = cacheModule[url];
+					depModule.factory(require, depModule.exports, depModule);
+					if (++idx == length) {
+						delete idx, depModule;
+						callback()
+					}
+				})
+			})
 		}
 	}
-	function request(url, callback, async) {
-		Q.getScript(uri, callback, async);
+	function load(id, callback, async) {
+		var url = id2url(id), module = cacheModule[url];
+		if (module) {
+			if (module.isReady) {
+				callback && callback(module.exports);
+			} else {
+				var idx = 0, depModule, dependencies = module.dependencies;
+				each(dependencies, function(i, v) {
+					request(v, function() {
+						depModule = cacheModule[v];
+						depModule.factory(require, depModule.exports, depModule);
+						if (++idx == dependencies.length) {
+							callback && callback(depModule.exports);
+							delete idx, depModule
+						}
+					})
+				})
+			}
+		} else {
+			request(url, function() {
+				module = cacheModule[url];
+				module.factory(require, module.exports, module);
+				callback && callback(module.exports)
+			})
+		}
 	}
-	function id2uri(id) {
-		id = alias2uri(id);
-		id = paths2uri(id);
-		id = vars2uri(id);
-		id = base + id;
-		return map2uri(id)
+	function request(url, callback) {
+		Q.log("request:" + url)
+		if (/\/\s*[^\/]*.css[?]?/.test(url)) {
+			var s = doc.createElement("link");
+			s.type = "text/css";
+			s.href = url;
+			Q(doc.head).append(s);
+			s.onload = callback;
+		} else {
+			Q.getScript(url, callback)
+		}
 	}
-	function alias2uri(id) {
-		return alias[id] || id;
+	function id2url(id) {
+		id = alias2url(id);
+		id = paths2url(id);
+		id = vars2url(id);
+		id = normalize(id);
+		return map2url(id)
 	}
-	function paths2uri(id) {
+	function normalize(url) {
+		if (!/^[a-zA-Z0-9]+:\/\//.test(url)) {
+			url = base + url
+		}
+		if (url.indexOf('?') === -1 && !/\.(css|js)$/.test(url)) {
+			url += '.js';
+		}
+		return url
+	}
+	function alias2url(id) {
+		return config.alias[id] || id;
+	}
+	function paths2url(id) {
 		var key = id.match(/^[0-9a-zA-Z.]+/);
 		key = key ? key[0] : id;
-		return id.replace(new RegExp("^" + key), paths[key] || key)
+		return id.replace(new RegExp("^" + key), config.paths[key] || key)
 	}
-	function vars2uri(id) {
+	function vars2url(id) {
 		var key = id.match(/\{[0-9a-zA-Z.]+\}/);
 		key = key ? key[0] : id;
-		return id.replace(new RegExp(key), vars[key] || key)
+		return id.replace(new RegExp(key), config.vars[key] || key)
 	}
-	function map2uri(id) {
-		each(map, function(i, v) {
+	function map2url(id) {
+		each(config.map, function(i, v) {
 			id.indexOf(v[0]) > -1 && id.replace(v[1])
 		});
 		return id
 	}
 	Q.sun = sun;
+	win.define = sun.define;
 	return sun
 })(Qmik);
