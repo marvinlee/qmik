83a217171341b913ea7c8a9200b26f01c26f5169
leo <cwq0312@163.com>
2013-05-09 13:03:46 +0800

    调整

--- test/testSun.js
+++ test/testSun.js
@@ -20,3 +20,62 @@
 		console.log("document onload ready:");
 	});
 })();
+Qmik.config("debug",true)
+Qmik.sun.config( {
+	alias : {
+		'mo' : 'http://127.0.0.1/js/mo.js'
+	}
+});
+Qmik.sun.define("ak", function(require, exports, module) {
+	var v = require("mo");
+	var b = require("mo");
+	var c = require("mo");
+	console.log("ak vvv mo:" + c+"--"+c());
+	function log() {
+		return 333;
+	}
+	module.exports=log
+	return log;
+});
+$(document).ready(function(Q) {
+	Qmik.sun.use("ak", function(ak) {
+		console.log("use ak:" + ak + ":"+ak());
+ 
+	})
+	var d=document.body;
+	document.getElementsBy
+	var dd=document.getElementsByTagName("div");
+	var d1=dd[0]
+	console.log("==:"+Q.contains(dd[1],d1));
+
+});
+
+function abc(a,b,c){
+	a("mm");
+	a("a");
+	
+	return a+b+c
+}
+function parseDepents(code) {
+	code=code.toString().replace(/\t?[\r\n]+/g," ");
+	var params = code.replace(/^\s*function\s*\w*\s*/, "").match(/^\([\w, ]*\)/)[0].replace("\(","").replace("\)","");;
+	var idx=params.indexOf(",");
+	if(idx==-1)return;
+	var require=params.substring(0,idx);
+	var pattern = new RegExp(require+"\s*[(]\s*[\"']([^\"'\)]+)[\"']\s*[)]","g");
+	var match=code.match(pattern)
+	console.log(match)
+}
+//parseDepents('(require, exports, module) { 	var v = require("mo"); 	console.log("ak vvv mo:" + v); 	function log() { 		return 333; 	} 	return log; } ');
+
+var m={name:"aaa"};
+m=Qmik.extend(m,{age:33});
+console.log(Qmik.stringify(m));
+
+Qmik.delay(function(a,b){
+	console.log("delay:"+a+":"+b);
+},500,10,20);
+var url="http://a.com/a.css?asdf/afasdf/er 中圖  /中圖.css?afa.css/asdf?qq/q.css/aa.js";
+var nurl=url.substring(0,url.indexOf("?")).trim();
+var isCSS = /\/.+\.css$/i.test(nurl);
+console.log("=============1:"+isCSS)
