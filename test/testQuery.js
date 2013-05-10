af5c7c9788f1cd69d9cf2a7fde075ae5fd77ea0d
leo <cwq0312@163.com>
2013-05-10 15:21:32 +0800

    调整

--- test/testQuery.js
+++ test/testQuery.js
@@ -1,22 +1,40 @@
-﻿;
-(function() {
-    //定义测试模块
-    module( "测试示例" );
-    //定义一个简单的函数，判断参数是不是数字
-    function simpleTest(para) {
-      if(typeof para == "number") {
-        return true;
-      }
-      else{
-        return false;
-      }
-    }
-    //开始单元测试
-    test('simpleTest()', function() {
-      //列举各种可能的情况，注意使用 ! 保证表达式符合应该的逻辑
-      ok(simpleTest(2), '2是一个数字');
-      ok(!simpleTest("2"), '"2"不是一个数字');
-    });
+﻿(function($) {
+	$.config("debug", true);
+	$.log("");
+	function capability(){
+		var old=$.now();
+		for(var i=0;i<50000;i++){
+			$("div ol>li[flag=p1]");
+		}
+		$.log("---"+($.now()-old));
+		if(!window.console)alert("---"+($.now()-old))
+	}
 
-
-})();
+	//查询测试
+	function testSelect(selector) {
+		module("查询语句:(" + selector+")");
+		//开始单元测试
+		test('queryTest()', function() {
+			//列举各种可能的情况，注意使用 ! 保证表达式符合应该的逻辑
+			ok(queryTest(selector), 'ok:'+selector);
+		});
+	}
+	//定义一个简单的函数，判断参数是不是数字
+	function queryTest(selector) {
+		$.log("查询语句:" + selector);
+		var length1 = $(selector).length, length2 = document.querySelectorAll(selector).length;
+		$.log("\t\t结果:" + selector + "," + length1 + "--" + length2);
+		return length1 == length2
+	}
+	testSelect("div");
+	testSelect("#part1");
+	testSelect("#part2 ol");
+	testSelect("#part2>ol>li");
+	testSelect("#part2 li");
+	testSelect("#part2 li[flag=p1]");
+	testSelect("#part2 li[flag=p2]");
+	testSelect("#part2>li[flag=p2]");
+	testSelect("#part2 li:hover");
+	
+	capability();
+})(Qmik);
