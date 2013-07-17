65b87d4eb94639a0be9d93e404edba5b065f895d
leo <cwq0312@163.com>
2013-07-17 22:04:40 +0800

    增加滑动

--- src/plugins/Qmik.fn.slider.js
+++ src/plugins/Qmik.fn.slider.js
@@ -31,7 +31,12 @@
 			left : ulPos.left,
 			top : ulPos.top,
 			current : lis.first(),
-			_site : 0,
+			_site : 0
+		});
+		$("#show2").html(me.width + "<>" + lis.length)
+		Q.extend(me, {
+			countWidth : me.width * lis.length,
+			countHeight : me.height * lis.length
 		});
 		//前面插入最后一个图像
 		me.first.before(lis.last().clone(true));
@@ -62,19 +67,52 @@
 	}
 	Q.extend(Silder.prototype, {
 		_initEvent : function() {
-			var me = this;
-			/*			me.container.on("touchstart", function(e) {
-							//e.stopPropagation();
-							//e.preventDefault();
-							//me.stop()
-						});*/
+			var me = this, conf = me.config, //
+			moveStart = 0, //
+			move = "next", //
+			isY = me.isY();
+			window.onerror = function(e) {
+				alert(e.message);
+			}
+			me.ul.on({
+				"touchstart" : function(e) {
+					me.stop();
+					if (me.isPrev()) {
+						length = isY ? me.height : me.width;
+						var countLength = isY ? me.countHeight : me.countWidth;
+						me.current.prev("li").length < 1 && me._initPosition(-countLength + length);
+					} else {
+						me.current.next("li").length < 1 && me._initPosition();
+					}
+					var touche = e.touches[0];
+					moveStart = isY ? touche.clientY : touche.clientX;
+					e.stopPropagation();
+					e.preventDefault();
+				},
+				"touchmove" : function(e) {
+					var touche = e.touches[0];
+					var diff = (isY ? touche.clientY : touche.clientX) - moveStart;
+					move = diff > 0 ? "prev" : "next";
+					//setCurrentByPos(me);
+					moveStart = touche.clientX;
+				},
+				"touchend" : function(e) {
+					if (move == "prev") {
+						me.prev()
+					} else {
+						me.next();
+					}
+					me.play();
+				}
+			});
 		},
 		_initPosition : function(mov) {
-			var me = this, mov = mov || 0, //
+			mov = mov || 0;
+			var me = this, //
 			translate = gTranslate + (me.isY() ? "Y" : "X") + "(" + mov + "px)";
 			me._site = mov;
 			me.ul.css(Q.cssPrefix({
-				"transition" : "0s",
+				"transition" : "0ms",
 				"transform" : translate
 			}));
 			me.current = me.isPrev() ? me.last : me.first
@@ -88,12 +126,14 @@
 		},
 		play : function() {
 			var me = this, conf = me.config;
+			me.thread && me.thread.stop();
 			me.thread = Q.cycle(function() {
 				me.isPrev() ? me.prev() : me.next()
 			}, conf.speed + conf.delay)
 		},
 		stop : function() {
-			this.thread.stop();
+			var me = this;
+			me.thread.stop();
 			me.ul.css(Q.cssPrefix({
 				"transition" : "0s"
 			}))
@@ -101,7 +141,7 @@
 		isToggle : function() {
 			return this.focus ? document.hasFocus() : !0
 		},
-		next : function() {
+		next : function(callback) {
 			var me = this, conf = me.config, //
 			isY = me.isY(), //
 			$tar = getNext(me), //
@@ -114,15 +154,17 @@
 						"transition" : conf.speed + "ms",
 						"transform" : translate + "(" + me._site + "px)"
 					}));
-					Q.delay(function() {
+					me._threadSite && me._threadSite.stop();
+					me._threadSite = Q.delay(function() {
 						me.current[0] == me.list.last()[0] && me._initPosition();
-					}, conf.speed + conf.delay - 50)
+						callback && callback(me);
+					}, conf.speed + conf.delay - 10)
 				}
 			} else {
 				me.stop()
 			}
 		},
-		prev : function() {
+		prev : function(callback) {
 			var me = this, conf = me.config, //
 			isY = me.isY(), //
 			$tar = getPrev(me), //
@@ -130,15 +172,17 @@
 			length = isY ? me.height : me.width;
 			if ($tar && $tar.length > 0) {
 				if (me.isToggle()) {
-					var countLength = length * (me.list.length - 2);
+					var countLength = isY ? me.countHeight : me.countWidth;
 					me._site += length;
 					me.ul.css(Q.cssPrefix({
 						"transition" : conf.speed + "ms",
 						"transform" : translate + "(" + me._site + "px)"
 					}));
-					Q.delay(function() {
+					me._threadSite && me._threadSite.stop();
+					me._threadSite = Q.delay(function() {
 						me.current[0] == me.list.first()[0] && me._initPosition(-countLength + length);
-					}, conf.delay)
+						callback && callback(me);
+					}, conf.speed + conf.delay - 10)
 				}
 			} else {
 				me.stop()
@@ -147,11 +191,22 @@
 	});
 	function getNext(me) {
 		me.current = me.current.next("li");
+		//me.current.length < 1 && me._initPosition();
 		return me.config.loop ? me.current : me.current[0] == me.list.last()[0] ? null : me.current
 	}
 	function getPrev(me) {
 		me.current = me.current.prev("li");
+		//me.current.length < 1 && me._initPosition();
 		return me.config.loop ? me.current : me.current[0] == me.list.first()[0] ? null : me.current
+	}
+	function getTransform(me) {
+		var transform = /-?\d+/.exec(me.ul.css(Q.cssPrefix("transform")));
+		return transform ? toInt(transform[0]) : 0;
+	}
+	function setCurrentByPos(me) {
+		var site = getTransform(me);
+		var idx = Math.floor(site / me.width) + 1;
+		me.current = me.list[idx];
 	}
 	Q.fn.extend({
 		silder : function(conf) {
