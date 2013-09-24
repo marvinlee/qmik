926f4fe4d54f4fc35b1a6ba4c1777ec044ffa567
leo <cwq0312@163.com>
2013-09-25 07:11:59 +0800

    增加jsonp请求方式,修改get请求的参数传递bug

--- src/Qmik._ajax.js
+++ src/Qmik._ajax.js
@@ -4,7 +4,9 @@
  * @version:1.00.000
  */
 (function(Q) { /* ajax */
-	var win = Q.global, toObject = Q.parseJSON, isFun = Q.isFun, ac = {
+	var win = Q.global, toObject = Q.parseJSON, isFun = Q.isFun, //
+	regUrl = /[\w\d_$-]+\s*=\s*\?/, jsonp = 1, prefex = "jsonp", //
+	ac = {
 		type : 'GET',
 		async : !0,
 		dataType : 'text'
@@ -13,11 +15,46 @@
 		return win.XMLHttpRequest && (win.location.protocol !== 'file:' || !win.ActiveXObject)	? new win.XMLHttpRequest()
 																															: new win.ActiveXObject('Microsoft.XMLHTTP')
 	}
+	//jsonp请求
+	function ajaxJSONP(_config, success, error) {
+		var ttl = _config.timeout, thread, isExe = 1, //
+		url = _config.url, gdata = Q.param(_config.data), //
+		callbackName = prefex + (jsonp++), //
+		cb = url.match(regUrl);
+		if (url.indexOf("?") < 0) url += "?";
+		if (cb) {
+			cb = cb[0].split("=")[0];
+			url = url.replace(regUrl, cb + "=" + callbackName)
+		} else {
+			url = url + "callback=" + callbackName
+		}
+		url += gdata;
+		function err() {
+			if (isExe == 1) {
+				isExe = 0;
+				Q("script[jsonp='" + callbackName + "']").remove();
+				error && error()
+			}
+		}
+		win[callbackName] = function(data) {
+			win[callbackName] = null;
+			Q("script[jsonp='" + callbackName + "']").remove();
+			thread && thread.stop();
+			success && success(data)
+		}
+		Q(Q.getScript(url, null, err)).attr("jsonp", callbackName);
+		if (ttl > 0) thread = Q.delay(err, ttl)
+	}
 	function ajax(conf) {
 		var _config = Q.extend({}, ac, conf), dataType = _config.dataType, ttl = _config.timeout, //
-		xhr = request(), data = _config.data, thread, success = _config.success, error = _config.error //
-		;
-		//_config.beforeSend && _config.beforeSend();
+		xhr = request(), url = _config.url, data = _config.data, isGet = _config.type == "GET", //
+		success = _config.success, error = _config.error, //
+		thread;
+		if (dataType == "jsonp") {
+			ajaxJSONP(_config, success, error);
+			return;
+		}
+		//ajax deal
 		xhr.onreadystatechange = function() {
 			if (4 == xhr.readyState) {
 				if (200 == xhr.status) {
@@ -25,13 +62,17 @@
 					success && success(dataType == 'xml' ? xhr.responseXML
 																	: (dataType == 'json' ? toObject(xhr.responseText) : xhr.responseText))
 				} else {
-					error && error(xhr)
+					error && error()
 				}
 			}
 		};
-		xhr.open(_config.type, _config.url, _config.async);
+		if (isGet) {
+			if (url.indexOf("?") < 1) url += "?";
+			url += "&" + Q.param(data)
+		}
+		xhr.open(_config.type, url, _config.async);
 		xhr.setRequestHeader("Cache-Control", "no-cache");
-		xhr.send(_config.type == "GET" ? Q.param(data) : data);
+		xhr.send(isGet ? {} : data)
 		if (ttl > 0) thread = Q.delay(function() {
 			xhr.abort();
 			error && error(xhr.xhr, xhr.type)
