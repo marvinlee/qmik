faff2996280adf7e8a5c6ea25224943ef3847c6b
leo <cwq0312@163.com>
2014-01-11 20:53:19 +0800

    增加图片司加载组件

---
+++ src/plugins/Qmik.fn.lazyImg.js
@@ -0,0 +1,59 @@
+/**
+ * 图片懒加载
+ */
+(function(Q) {
+	var win = window;
+	//是否在视口位置中
+	function inViewport(dom) {
+		var min = win.pageYOffset - Q(dom).height() * 2;
+		var max = win.pageYOffset + win.innerHeight;
+		var _top = Q(dom).offset().top;
+		console.log(min+":"+max+":"+_top)
+		return _top >= min && _top <= max;
+	}
+
+	function bindEvent() {
+		var loadIndex=0;
+		Q(win).on("scroll", function() {
+			var oldIndex = loadIndex;
+			loadIndex++;
+			var lazys = Q('img.lazy');
+			for (var i = 0; i < lazys.length; i++) {
+				if (oldIndex + 1 != loadIndex) {
+					break;
+				}
+				var node = lazys[i];
+				var Qme = Q(node);
+				if (inViewport(Qme)) {
+					load(Qme);
+				}
+			}
+		}).trigger("scroll");
+	}
+
+	function load(me) {
+		if (me.attr('loaded')) return;
+		var src = me.attr('_src');
+		if (src) {
+			var img = new Image();
+			img.onload = function() {
+				me.attr('src', src).removeClass('lazy');
+				me.css({
+					"transition": "360ms",
+					"opacity": "1"
+				});
+			}
+			img.src = src;
+			me.attr('loaded', true);
+		}
+	}
+
+	function lazyImg(opt) {
+		Q('img.lazy').css("opacity", "0.8");
+		bindEvent();
+	}
+	Q.fn.lazyImg = lazyImg;
+	Q.sun.define(function(require, exports, module) {
+		module.exports = Q;
+	});
+})($);
