223fe5bce25772a56a2c87fc0a96ac1a464d592e
leo <cwq0312@163.com>
2014-08-21 22:53:08 +0800

    add

--- test/sun/demo2.js
+++ test/sun/demo2.js
@@ -2,8 +2,7 @@ $.define("demo/Demo2",function(require, exports, module){
 	
 	module.exports = {
 		doing:function(msg){
-			var Loadimg = require("http://g.tbcdn.cn/tmmob/lib-loadimg/1.0.0/loadimg.js");
-			var Store = require("http://g.tbcdn.cn/tmmob/lib-store/2.0.0/store.js");//require("34")
+
 
 			var Demo = require("demo/Demo1");
 			var Demo3 = require("demo/Demo3");
