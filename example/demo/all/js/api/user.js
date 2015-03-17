a951e567b07685311ed957b97500381ca77f1c98
leo <cwq0312@163.com>
2015-03-17 22:04:53 +0800

    增加模块化+mvc开发demo  example/demo/all

---
+++ example/demo/all/js/api/user.js
@@ -0,0 +1,31 @@
+/**
+ * 用户基础接口
+ */
+(function ($) {
+    var Module = {
+        /**
+         *[callback]
+         * @param callback
+         */
+        "get": function (callback) {
+            //通过ajax向后台取数据,这边模块延迟回调
+            $.delay(function () {
+                var result = {
+                    code: 1,
+                    data: {
+                        id: 1,
+                        name: "leo@qmik.org",
+                        nick: "leo",
+                        qq: "qq",
+                        email: "leo@qmik.org"
+                    }
+                };
+                callback && callback(result, result.data);
+            }, 500)
+        }
+    };
+
+    $.define("api/user", function (require, exports, module) {
+        module.exports = Module;//导出模块
+    });
+})(Qmik);
---
+++ example/demo/all/js/view/demo.js
@@ -0,0 +1,42 @@
+Qmik(function ($) {
+    //$.app() 编译并生成应用,$.app().ctrl()定义控制器
+
+    $.app().ctrl({
+        user: function (scope) {
+            $.use("api/user", function (User) {
+                User.get(function (result, user) {
+                    scope.user = user;
+                    scope.apply(["user"]);//更新
+                });
+            });
+        },
+        shop: function (scope) {//购物
+            $.use("api/shop", function (Shop) {
+                Shop.get(function (result, shop) {
+                    scope.shop = shop;
+                    scope.apply(["shop"]);
+                });
+
+                scope.clickme = function (e) {
+                    scope.msg = "我变了 " + parseInt($.now() / 1000 + Math.random() * 100) + '';
+                    scope.apply(["msg", "gohome"]); //这种写法,性能比较好,保更新 msg 对应的节点信息
+                    //scope.apply();//这种写法性能比较差,会更新整个控制器的所有节点信息,
+                }
+
+                //倒计时
+                var ttl = 5000;//
+                scope.time = parseInt(ttl / 1000);
+                var startTime = $.now() + ttl;
+                $.cycle(function () {
+                    var t = startTime - $.now();
+                    scope.time = parseInt(t / 1000);
+                    scope.apply("time");
+                    if (scope.time < 1) {
+                        scope.$(".load").hide();
+                    }
+                }, 1000, ttl);
+
+            });
+        }
+    });
+});
